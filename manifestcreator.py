f8fa853a6b865411b6f5feaa058c1c2d81b63333
Elliot Jordan <elliot@elliotjordan.com>
2016-04-22 09:13:07 -0700

    Tabs to spaces.

--- manifestcreator.py
+++ manifestcreator.py
@@ -23,6 +23,6 @@ p.add_argument("file")
 arguments = p.parse_args()
 
 with open(arguments.file, 'rb') as f:
-	reader = csv.reader(f)
-	for row in reader:
-		shutil.copyfile('/Volumes/munki/manifests/Template','/Volumes/munki/manifests/' + row[0])
+    reader = csv.reader(f)
+    for row in reader:
+        shutil.copyfile('/Volumes/munki/manifests/Template', '/Volumes/munki/manifests/' + row[0])
